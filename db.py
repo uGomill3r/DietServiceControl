f8e3493a50500751f5be31d396eb7ccae97d6238
devop <evoluxiperu@gmail.com>
2025-10-03 23:36:44 -0500

    Configuración para base de datos de Neon.

--- db.py
+++ db.py
@@ -6,10 +6,11 @@ import os
 load_dotenv()
 
 def get_connection():
-    return psycopg2.connect(
-        dbname=os.getenv("DB_NAME"),
-        user=os.getenv("DB_USER"),
-        password=os.getenv("DB_PASSWORD"),
-        host=os.getenv("DB_HOST"),
-        port=os.getenv("DB_PORT")
-    )
+    db_name = os.environ['DB_NAME']
+    db_user = os.environ['DB_USER']
+    db_password = os.environ['DB_PASSWORD']
+    db_host = os.environ['DB_HOST']
+    db_port = os.environ.get('DB_PORT', '5432')  # usa 5432 por defecto
+
+    conn_str = f"dbname={db_name} user={db_user} password={db_password} host={db_host} port={db_port} sslmode=require"
+    return psycopg2.connect(conn_str)
